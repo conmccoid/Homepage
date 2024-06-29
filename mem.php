fb84c1b1dd73c115a74be38b1861a7e7de421067
conmccoid <conmccoid@gmail.com>
2024-06-29 16:13:08 -0400

    updated MEM blog
    updating with changes to MEM project, including first ANN results (see MozartExMachina repo)

--- mem.php
+++ mem.php
@@ -107,6 +107,37 @@ dd {
       midi_data.parts[0].measure(1).getElementsByClass('MetronomeMark').show('text') # tempo(s)
     </code></pre>
 
+    <p>To extract notes from `midi_data`, we can recurse into each part and measure.</p>
+    <pre><code class="language-python">
+      data=midi_data.recurse().notes
+    </code></pre>
+    <p>
+      We now have the master list of notes for the entire song.
+      The length of this list is the number of notes: `N=len(data)`.
+      Each entry is one note or chord, complete with several relevant pieces of information.
+      We are interested in three in particular: offset, pitch, and duration.
+    </p>
+    <p>
+      Before we extract these data points, we need to separate chords into individual notes.
+      Suppose `data[i]` is a chord, then it is stored as a list of notes.
+      We can go through each note in the chord individually.
+    </p>
+    <pre><code class="language-python">
+    def read_chord(chord,group_offset):
+      output=[]
+      if type(chord)==music21.note.Note:
+          output.append(chord.pitch.ps)
+          output.append(chord.duration.quarterLength)
+          output.append(chord.offset - group_offset)
+      elif type(chord)==music21.chord.Chord:
+          for k in range(len(chord.notes)):
+              noteChord=chord.notes[k]
+              output.append(noteChord.pitch.ps)
+              output.append(noteChord.duration.quarterLength)
+              output.append(noteChord.offset - group_offset)
+      return output
+    </code></pre>
+
     <h3>Feature space</h3>
     <p>To feed data into an ANN, we need to divide each datum into features. For this project, each datum is a
       selection of music. Since the music is stored as a sequence of chords, each with a set of notes, an offset,
@@ -115,14 +146,17 @@ dd {
       as well as the instrument played and other important details.
     </p>
     <p>The size of each datum can be as small as a single chord or as long as the entire song. However, it must
-      be constant. Let the number of chords found in each datum be cpd (Chords Per Datum). Each song must then
-      be divided into data by taking cpd chords at a time.
+      be constant.
     </p>
-    <p>The songs as found in the dataset are divided into measures. This suggests we should use measures as the 
-      length of each sample. Note, however, that the feature space will become complicated, as the contents of 
-      each measure vary significantly. Can each measure be fed into an ANN as is? How can we annotate such a high 
-      dimensional sample? What to do with strictly qualitative features?
+    <p>Notes and their durations and offsets are easily described by numbers.
+      Chords, however, are not easily reduced to an ordinal format.
+      The complexity of representing chords is rich enough for its own project.
+      To sidestep this, we divide each chord into its constituent notes and take as datum sets of n notes.
     </p>
+    <p>
+      Thus, we are using 3*n features: for each note in the datum, there is its pitch space, its quarter length, and its offset relative to the first note in the sequence.
+    </p>
+
 
     <h3>Classifier: <b>Name That Tune</b></h3>
     <p>Let's begin with an easy task for scikit-learn to handle: classifying a measure as belonging to a specific
@@ -132,6 +166,101 @@ dd {
       NTT takes a single datum and returns the song title. I have 21 songs composed by Mozart as a dataset. The 
       available classes should then be these 21 songs. Note that each 'song' is in fact a movement in a larger 
       piece. Ideally, this information can also be encoded into NTT.
+    </p>
+    <p>
+      To preprocess the dataset for this portion of the project, we'll need to split up each song, classify each portion, then feed it into one of scikit-learn's classifiers.
+      The following function parses an input song and splits up its collection of notes into sequences of n notes using the `read_chord()` function from earlier.
+    </p>
+    <pre><code class=language-ptyhon">
+    def read_noteGroups(file,n):
+      midi_data=music21.converter.parse(file)
+      data=midi_data.recurse().notes
+      N=len(data)
+      output=[]
+      for i in range(N-n):
+          group_offset=data[i].offset
+          group=[]
+          for j in range(n):
+              chord_out=read_chord(data[i+j],group_offset)
+              for note_out in chord_out:
+                  group.append(note_out)
+          output.append(group[0:3*n])
+      return output
+    </code></pre>
+    <p>The data is read into Python using the following code.</p>
+    <pre><code>
+      import music21
+      X=[]; y=[]; i=0;
+      for ind in ['311','330','331','332','333','545','570']:
+      # for ind in ['311']:
+          i+=1
+          for j in ['_1','_2','_3']:
+              pitches_output=read_noteGroups('mozart/mz_'+ind+j+'.mid',20)
+              num_output=len(pitches_output)
+              for pitch_out in pitches_output:
+                  X.append(pitch_out)
+                  y.append(i)
+    </code></pre>
+
+    <p>
+      We'll use scikit-learn's RandomForestClassifier, a random forest model for classifying samples into qualitative groups.
+      A random forest model uses a collection of decision trees, with each node on the tree a binary split based on a single variable extracted from the feature space.
+      (That is to say, this variable could be a function of many features.)
+      As the name suggests, each tree is random, in the sense that the variable chosen at each node is selected randomly.
+      The binary split is then chosen based on the data for this variable.
+    </p>
+    <p>
+      For example, consider a computer vision classifier for gender.
+      A decision node on a tree in the random forest may use hair length as its variable.
+      If hair length correlates strongly with female, then this decision node will split those with long hair into the female category and those with short hair into the male category.
+      The tree may continue to grow, adding more decision nodes to further refine its categorization.
+    </p>
+    <p>There are four main parameters to tweak for this model:</p>
+    <li>
+      <i>n-estimators: number of trees to make (default 100);</i>
+      <i>max_features: number of features to consider at each split;</i>
+      <i>max_depth: maximum number of nodes in each tree;</i>
+      <i>min_samples_split: number of samples split off by each node.</i>
+    </li>
+    <p>
+      Setting max_depth to None and min_samples_split to 2 results in fully developed trees (all samples split, trees as large as it takes).
+      Standard practice is to set max_features to 'sqrt' for classification problems like ours.
+    </p>
+    <p>
+      To use RandomForestClassifier, start by initializing the classifier, then feeding it data.
+      The list of datum is stored in `X`, an array with two dimensions.
+      Each row (first dimension) of `X` is a datum, and each element in this row (second dimension) is a feature of the datum.
+      The list of classifications of each datum is stored in `y`, a 1D array, so that the i-th element in `y` classifies the i-th row of `X`.
+    </p>
+    <p>Data is first split into training and testing sets.</p>
+    <pre><code>
+      from sklearn.model_selection import train_test_split
+      X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=0)
+    </code></pre>
+    <p>We now have datasets to plug into our `RandomForestClassifier()`, which we now need to construct.</p>
+    <pre><code>
+    from sklearn.ensemble import RandomForestClassifier
+    clf = RandomForestClassifier(n_estimators=100, max_features='sqrt', max_depth=None, min_samples_split=2)
+    clf.fit(X_train,y_train)
+    </code></pre>
+    <p>
+      With the classifier trained, all that remains is to test it against the test dataset.
+      This will give a percentage of the remaining data that the trained classifier gets right.
+      A number closer to 1 indicates the classifier is good at classifying these note sequences, while a number closer to 0 indicates it cannot correctly associate note sequences with particular songs.
+      With random guesses, a random classifier has a score of 1/m, where m is the number of possible classifications.
+      Therefore, a classifier must be better than this to be considered useful.
+      In our case, that number is 1/21, or 4.8%.
+    </p>
+    <pre><code>
+    from sklearn.metrics import accuracy_score
+    print(accuracy_score(clf.predict(X_test),y_test))
+    </code></pre>
+    <p>
+      I chose to take sequences of 20 notes, resulting in several thousand sequences.
+      These had 60 features.
+      The resulting score was 87.3%.
+      I found that the choice of `max_features` was particularly critical to the accuracy.
+      Even taking longer note sequences, if the classifier could not collect more features, then it was as if these sequences were signficantly shorter.
     </p>
 </div>
 
